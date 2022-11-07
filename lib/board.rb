47c2451152aea73fd014184235ffb98a65932a97
nfrank5 <nfrankenfeld@gmail.com>
2022-11-07 18:51:30 +0800

    Add check_winner_rows method and tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,29 @@
 class Board
-  
+  ROWS = 6
+  COLUMNS = 7
+  attr_reader :board
 
-  def check_winner_rows
-
+  def initialize(board = Array.new(ROWS){Array.new(COLUMNS)} )
+    @board = board
   end
 
+  def check_winner_rows
+    board.each_with_index do | row, row_index |
+      count = 0
+      row.each_with_index do | cell, cell_index |
+        if cell == board[row_index][cell_index - 1] && !cell.nil? && cell_index > 0
+          count += 1
+        else
+          count = 0
+        end 
+      end
+      return true if count >= 3
 
+    end
+    false
+  end
+
+  
 
 end
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,15 +2,45 @@ require_relative '../lib/board'
 
 describe Board do
   describe "#check_winner_rows" do
-    subject(:board_check_winner_rows) { Board.new }
-    context 'when there are four one player consecutive discs aligned in the first row' do
-
+    context 'when there are four one player consecutive discs aligned in a row' do
+      board = [ [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, 'O', 'O', 'O', 'O'],
+                [nil, nil, nil, nil, nil, nil, nil]]
+      
+      subject(:board_check_winner_rows) { Board.new(board) }
       it 'returns true' do
         expect(board_check_winner_rows.check_winner_rows).to be true
       end
     end
+    context 'when there are four or more one player discs aligned in a row but not consecutive' do
+      board = [ [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil],
+                ['O', 'O', 'O', nil, 'O', 'O', 'O'],
+                [nil, nil, nil, nil, nil, nil, nil]]
+      
+      subject(:board_check_winner_rows) { Board.new(board) }
+      it 'returns false' do
+        expect(board_check_winner_rows.check_winner_rows).to be false
+      end
+    end
+    context 'when all the board is empty' do
 
-
+      board = [ [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil], 
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil]]
+      subject(:board_check_winner_rows) { Board.new(board) }
+      it 'returns false' do
+        expect(board_check_winner_rows.check_winner_rows).to be false
+      end
+    end
   end
 end
 
